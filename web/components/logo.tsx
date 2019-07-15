a9bc0b1afdc4dba9259f4a721f292a1a63160ba1
Ali Zahid <ali.zahid@live.com>
2019-07-16 01:08:41 +0400

    WEB Logo updated

--- web/components/logo.tsx
+++ web/components/logo.tsx
@@ -10,7 +10,7 @@ const Logo: FunctionComponent<Props> = ({ className }) => {
   return (
     <header className={`logo__main ${className}`}>
       <img className="logo__mesenja" src="/static/img/mesenja.svg" />
-      <h1>Mesenja</h1>
+      <h1 className="logo__name">Mesenja</h1>
     </header>
   )
 }
